2b31e6cdd4a67092b0cca2db5379d9a38548136a
Raymel Francisco <franciscoraymel@gmail.com>
2016-12-20 00:14:07 +0800

    Debug

--- VolPI/Form1.Designer.cs
+++ VolPI/Form1.Designer.cs
@@ -29,11 +29,35 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Text = "Speakers: ";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.Click += new System.EventHandler(this.notifyIcon1_Click);
+            this.notifyIcon1.DoubleClick += new System.EventHandler(this.notifyIcon1_DoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(157, 0);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "VolPI";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
     }
 }
 
--- VolPI/Form1.cs
+++ VolPI/Form1.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using AudioSwitcher.AudioApi.CoreAudio;
+using System.Drawing;
+using System.Diagnostics;
 
 namespace VolPI
 {
@@ -14,7 +10,93 @@ namespace VolPI
     {
         public Form1()
         {
-            InitializeComponent();
+            this.InitializeComponent();
+            this.notifyIcon1.Icon = generateNotifyIcon();
+            
+            this.notifyIcon1.Text += defaultPlaybackDevice.Volume + "%";
+            this.Hide();
         }
+
+        #region Events
+        private void notifyIcon1_Click(object sender, EventArgs e)
+        {
+            showMasterMixer();
+        }
+
+        /// <summary>
+        /// mutes master audio device when notify icon is double clicked
+        /// </summary>
+        private void notifyIcon1_DoubleClick(object sender, EventArgs e)
+        {
+            if (defaultPlaybackDevice.IsMuted)
+            {
+                defaultPlaybackDevice.Mute(false);
+            }
+            else
+            {
+                defaultPlaybackDevice.Mute(true);
+            }
+        }
+        #endregion
+
+        #region Class Methods
+        /// <summary>
+        /// shows volume adjuster/master mixer window position above the tray notification icon
+        /// </summary>
+        private void showMasterMixer()
+        {
+            this.Hide();
+
+            this.Left = Cursor.Position.X;
+            this.Top = Screen.PrimaryScreen.WorkingArea.Bottom - Height;
+
+            Process volTrack = new Process();
+            volTrack.StartInfo.WindowStyle = ProcessWindowStyle.Normal;
+            volTrack.StartInfo.FileName = "sndvol.exe";
+
+            int notifyIconPos = this.Top * 65536 + this.Left;
+
+            volTrack.StartInfo.Arguments = "-f " + notifyIconPos.ToString();
+            volTrack.Start();
+        }
+
+        /// <summary>
+        /// Generates custom text-like tray icon showing the current master volume percentage
+        /// </summary>
+        /// <returns>icon generated (Icon object)</returns>
+        private Icon generateNotifyIcon()
+        {
+            using (Bitmap bitmap = new Bitmap(16, 16))
+            {
+                using (Graphics graphics = Graphics.FromImage(bitmap))
+                {
+                    graphics.TextRenderingHint = System.Drawing.Text.TextRenderingHint.SingleBitPerPixelGridFit;
+                    graphics.DrawIcon(Properties.Resources.notifyIconTemplate, 0, 0);
+                    graphics.DrawString(this.defaultPlaybackDevice.Volume.ToString(), drawFont, drawBrush, 1, 2);
+
+                    using (Icon createdIcon = Icon.FromHandle(bitmap.GetHicon()))
+                    {
+                        return createdIcon;
+                    }
+                }
+            }
+        }
+        #endregion
+
+        #region Class Fields
+
+        /// <summary>
+        /// volume control object
+        /// courtesy of Audio Switcher Core Audio 3.0.0.1 API (NuGet package)
+        /// </summary>
+        CoreAudioDevice defaultPlaybackDevice = new CoreAudioController().DefaultPlaybackDevice;
+
+        /// <summary>
+        /// dynamic custom notify icon stuff
+        /// </summary>
+        private Font drawFont = new Font("Arial", 8, FontStyle.Regular);
+        public static SolidBrush drawBrush = new SolidBrush(Color.White);
+
+        #endregion
     }
 }
